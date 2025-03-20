e25497e0b64fc490ee373539345860a86f9a3af6
Vamshi-Raghuram-G <95559534+Vamshi-Raghuram-G@users.noreply.github.com>
2025-03-20 17:58:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 100) {
+        section.classList.add("visible");
+      }
+    });
+  });
+  
